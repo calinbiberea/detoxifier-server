517ccbec40d0e596df8d3d0b8bdf05e0b01691a8
Stroia Dacian <zademnwalves@gmail.com>
2021-01-24 11:15:15 +0200

    works local

--- app.py
+++ app.py
@@ -5,16 +5,21 @@ from os import walk
 from flask import Flask, request
 from flask_cors import CORS, cross_origin
 
+from utils import fake_predict
+
 app = Flask(__name__)
 CORS(app)
 CORS(app, resources={r"/*": {"origins": "*"}})
 app.config['CORS_HEADERS'] = 'Content-Type'
 
 
-@app.route('/post', methods=['POST'])
+@app.route('/post/<comment>', methods=['GET'])
 @cross_origin()
-def post_data():
-    return "Update has been successful!"
+def post_data(comment):
+    #comment = request.args.comment
+    res = fake_predict(comment)
+    print(comment, res)
+    return {'prediction': res}
 
 
 # A welcome message to test our server
---
+++ utils.py
@@ -0,0 +1,15 @@
+import pickle as pkl
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import TfidfTransformer
+from sklearn.pipeline import Pipeline
+import numpy as np
+
+
+with open('RF.pkl', 'rb') as f:
+    pipe = pkl.load(f)
+
+
+def fake_predict(comment):
+    res = pipe.predict([comment])
+    return res[0]
